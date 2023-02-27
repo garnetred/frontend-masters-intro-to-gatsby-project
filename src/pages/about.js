f3b4354ff84f438775ad1be7f00d24b61f5c74d4
December G <dgarnetsmith@gmail.com>
2023-02-27 01:34:17 -0500

    Create initial layout

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,11 +1,18 @@
 import * as React from 'react';
+import { Seo } from '../components/seo';
 import { Link } from 'gatsby';
 
 export default function AboutPage() {
   return (
-    <div>
-      <h2>About This Site</h2>
-      <Link to="/">Go home</Link>
-    </div>
+    <>
+      <Seo
+        title="About This Site"
+        description="More information about this site."
+      />
+      <main>
+        <h2>About This Site</h2>
+        <Link to="/">Go home</Link>
+      </main>
+    </>
   );
 }
